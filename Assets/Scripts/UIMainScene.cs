ae31cfc712974f2cced3761d62cfda7fdd095e04
Max Neijndorff <m.neijndorff1999@gmail.com>
2018-06-13 10:00:33 +0200

    added the unity project

---
+++ Assets/Scripts/UIMainScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainScene : MonoBehaviour {
+    GameObject Menubtn;
+    public GameObject MenuUI;
+
+    public bool isPaused = false;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Menu()
+    {
+        if (isPaused)
+        {
+            Resume();
+        }
+        else{
+            Pause();
+        }
+    }
+
+
+    public void Resume()
+    {
+        MenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        MenuUI.SetActive(true);
+        isPaused = true;
+    }
+}
